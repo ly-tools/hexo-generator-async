e41e43dec838b127d377362dd4ddcf0d62111782
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-06-06 01:06:11 +0800

    增加列表

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var assign = require('object-assign');
 var path = require('path');
+var _ = require('lodash');
 
-hexo.config.async = assign({
-  per_page: hexo.config.per_page || 10,
-  base: (hexo.config.async_generator && hexo.config.async_generator.base) ||  path.join(path.sep, 'async', path.sep)
-}, hexo.config.async_generator);
+hexo.config.async_generator = _.assign(hexo.config.async_generator, {
+  perPage: hexo.config.per_page || 10,
+  base: (hexo.config.async_generator && hexo.config.async_generator.base) ||  path.join(path.sep, 'async', path.sep),
+  url: hexo.config.url
+});
 
 
 hexo.extend.generator.register('async', require('./lib/async'));
