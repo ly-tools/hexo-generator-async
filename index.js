c03739faa3515ee4398c382967ce666b4e20195a
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-07-18 19:40:07 +0800

    fix bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var _ = require('lodash');
 
-hexo.config.async_generator = _.assign(hexo.config.async_generator, {
+hexo.config.async_generator = _.assign({}, hexo.config.async_generator, {
   perPage: hexo.config.per_page || 10,
   base: (hexo.config.async_generator && hexo.config.async_generator.base) ||  path.join(path.sep, 'async', path.sep),
   url: hexo.config.url
